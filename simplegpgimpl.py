b81f07e2e277c8e4ceadec7b4e91c619129ba2f2
kabl <kabl@digitalizeit.io>
2021-04-11 22:28:14 +0200

    refactoring

--- simplegpgimpl.py
+++ simplegpgimpl.py
@@ -11,12 +11,25 @@ STYLE_ERR = "fg:#f44336"
 class SimpleGPG:
 
     def __init__(self, home_dir):
+
         if not os.path.isdir(home_dir):
-            questionary.print("Directory does not exists:" + home_dir, style=STYLE_ERR)
-            sys.exit(1)
+            self.not_existing_directory(home_dir)
+
         self.gpg = gnupg.GPG(gnupghome=home_dir)
         self.gpg.encoding = 'utf-8'
-        os.system('cls||clear')
+
+    def not_existing_directory(self, home_dir):
+        question = "Directory '" + home_dir + "' does not exists. Create a directory?"
+        result = questionary.confirm(question).ask()
+        if result:
+            try:
+                os.mkdir(home_dir)
+                questionary.print("Directory created. No keypairs configured.", style=STYLE_TXT)
+            except OSError:
+                questionary.print("Could not create directory", style=STYLE_ERR)
+                sys.exit(1)
+        else:
+            sys.exit(0)
 
     def main_menu(self):
         choices = {"Key Management": self.key_management,
@@ -31,6 +44,9 @@ class SimpleGPG:
             questionary.print("### SIMPLEGPG - MENU ###", style=STYLE_MENU)
             questionary.print("")
             selection = questionary.select("Select your task", choices=choices.keys(), use_shortcuts=True).ask()
+            if selection is None:
+                return
+
             choices[selection]()
 
     def key_management(self):
@@ -47,7 +63,7 @@ class SimpleGPG:
         choices[selection]()
 
     def create_keypair(self):
-        print("Creating a new ID/Key")
+        questionary.print("Creating a new ID/Key", style=STYLE_TXT)
         name = questionary.text("What's your fullname?").ask()
         email = questionary.text("What's your e-mail address?").ask()
         length_options = ["1024", "2048", "4096"]
@@ -67,6 +83,9 @@ class SimpleGPG:
 
     def import_public_key(self):
         key = questionary.text("Enter public key to import:", multiline=True).ask()
+        if key is None:
+            return
+
         import_result = self.gpg.import_keys(key)
         if import_result.count == 0:
             questionary.print(import_result, style=STYLE_ERR)
@@ -156,8 +175,9 @@ class SimpleGPG:
 
         # TODO: Check OK Value after Signing
         signature = self.gpg.sign(message, keyid=signer_key)
-        questionary.print(signature, style=STYLE_TXT)
-        self.add_to_clipboard(signature)
+        signature_string = str(signature)
+        questionary.print(signature_string, style=STYLE_TXT)
+        self.add_to_clipboard(signature_string)
 
     def verify_signature(self):
         message = questionary.text("Enter message to verify:", multiline=True).ask()
@@ -168,7 +188,7 @@ class SimpleGPG:
         if result:
             questionary.print("Valid data", style=STYLE_TXT)
         else:
-            questionary.print(result.status, style=STYLE_ERR)
+            questionary.print("Message could not be verified!", style=STYLE_ERR)
 
     def select_id(self):
         choices = {}
@@ -176,6 +196,10 @@ class SimpleGPG:
         for pub_key in public_keys:
             for uid in pub_key['uids']:
                 choices[uid] = pub_key['keyid']
+
+        if not choices.keys():
+            questionary.print("No keys configured. Create a keypair.", style=STYLE_ERR)
+            return
 
         selection = questionary.select("Which ID do you want to use?", choices=choices.keys(), use_shortcuts=True).ask()
         if selection is None:
@@ -190,6 +214,10 @@ class SimpleGPG:
         for pub_key in public_keys:
             for uid in pub_key['uids']:
                 choices[uid] = pub_key['keyid']
+
+        if not choices.keys():
+            questionary.print("No Public Keys available. Import Public Key.", style=STYLE_ERR)
+            return
 
         selection = questionary.select("Select Recipient", choices=choices.keys(), use_shortcuts=True).ask()
         if selection is None:
